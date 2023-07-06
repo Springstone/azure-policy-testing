6b78fa996a829727130599cd72f4146335592a1c
Sacha Narinx <Springstone@users.noreply.github.com>
2023-07-06 19:48:48 +0400

    .

--- tests/Deny-MgmtPorts-From-Internet.Tests.ps1
+++ tests/Deny-MgmtPorts-From-Internet.Tests.ps1
@@ -88,5 +88,32 @@ Describe "Testing policy 'Deny-MgmtPorts-From-Internet'" -Tag "deny-mgmtports-fr
                 } | Should -Throw "*disallowed by policy*"
             }
         }
+
+        It "Should allow compliant port ranges" -Tag "deny-route-nexthopvirtualappliance-nsg-port-40" {
+            AzTest -ResourceGroup {
+                param($ResourceGroup)
+
+                $networkSecurityGroup = New-AzNetworkSecurityGroup `
+                -Name "nsg-test" `
+                -ResourceGroupName $ResourceGroup.ResourceGroupName `
+                -Location $ResourceGroup.Location
+
+                # Should be disallowed by policy, so exception should be thrown.
+                {
+                    $networkSecurityGroup | Add-AzNetworkSecurityRuleConfig `
+                        -Name RDP-rule `
+                        -Description "Allow RDP" `
+                        -Access Allow `
+                        -Protocol Tcp `
+                        -Direction Inbound `
+                        -Priority 200 `
+                        -SourceAddressPrefix * `
+                        -SourcePortRange * `
+                        -DestinationAddressPrefix * `
+                        -DestinationPortRange ["80","443"] # Incompliant.
+                    | Set-AzNetworkSecurityGroup
+                } | Should -Not -Throw
+            }
+        }
     }
 }
