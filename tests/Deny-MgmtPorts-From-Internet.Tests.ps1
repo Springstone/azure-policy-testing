bf6d4b6336f26b86b5185df21f9394950b84809f
Sacha Narinx <Springstone@users.noreply.github.com>
2023-07-10 15:33:45 +0400

    .

--- tests/Deny-MgmtPorts-From-Internet.Tests.ps1
+++ tests/Deny-MgmtPorts-From-Internet.Tests.ps1
@@ -8,7 +8,7 @@ Import-Module "$($PSScriptRoot)/../utils/Test.Utils.psm1" -Force
 Describe "Testing policy 'Deny-MgmtPorts-From-Internet'" -Tag "deny-mgmtports-from-internet" {
     # Create or update NSG is actually the same PUT request, hence testing create covers update as well.
     Context "When NSG is created or updated" -Tag "deny-mgmtports-from-internet-nsg-port" {
-        It "Should deny non-compliant port '3389'" -Tag "deny-route-nexthopvirtualappliance-nsg-port-10" {
+        It "Should deny non-compliant port '3389'" -Tag "deny-noncompliant-nsg-port-10" {
             AzTest -ResourceGroup {
                 param($ResourceGroup)
 
@@ -35,7 +35,7 @@ Describe "Testing policy 'Deny-MgmtPorts-From-Internet'" -Tag "deny-mgmtports-fr
             }
         }
 
-        It "Should deny non-compliant port '3389' inline" -Tag "deny-route-nexthopvirtualappliance-nsg-port-10" {
+        It "Should deny non-compliant port '3389' inline" -Tag "deny-noncompliant-nsg-port-20" {
             AzTest -ResourceGroup {
                 param($ResourceGroup)
 
@@ -60,7 +60,7 @@ Describe "Testing policy 'Deny-MgmtPorts-From-Internet'" -Tag "deny-mgmtports-fr
             }
         }
 
-        It "Should deny non-compliant port '22'" -Tag "deny-route-nexthopvirtualappliance-nsg-port-20" {
+        It "Should deny non-compliant port '22'" -Tag "deny-noncompliant-nsg-port-30" {
             AzTest -ResourceGroup {
                 param($ResourceGroup)
 
@@ -87,7 +87,7 @@ Describe "Testing policy 'Deny-MgmtPorts-From-Internet'" -Tag "deny-mgmtports-fr
             }
         }
 
-        It "Should deny non-compliant port ranges (21-23)" -Tag "deny-route-nexthopvirtualappliance-nsg-port-30" {
+        It "Should deny non-compliant port ranges (21-23)" -Tag "deny-noncompliant-nsg-port-40" {
             AzTest -ResourceGroup {
                 param($ResourceGroup)
 
@@ -114,7 +114,7 @@ Describe "Testing policy 'Deny-MgmtPorts-From-Internet'" -Tag "deny-mgmtports-fr
             }
         }
 
-        It "Should allow compliant ports (443)" -Tag "deny-route-nexthopvirtualappliance-nsg-port-40" {
+        It "Should allow compliant ports (443)" -Tag "allow-compliant-nsg-port-10" {
             AzTest -ResourceGroup {
                 param($ResourceGroup)
 
@@ -141,7 +141,7 @@ Describe "Testing policy 'Deny-MgmtPorts-From-Internet'" -Tag "deny-mgmtports-fr
             }
         }
 
-        It "Should deny non-compliant port ranges (Test)" -Tag "deny-route-nexthopvirtualappliance-nsg-port-50" {
+        It "Should deny non-compliant port ranges (Test)" -Tag "deny-noncompliant-nsg-port-50" {
             AzTest -ResourceGroup {
                 param($ResourceGroup)
 
@@ -168,7 +168,7 @@ Describe "Testing policy 'Deny-MgmtPorts-From-Internet'" -Tag "deny-mgmtports-fr
             }
         }
 
-        It "Should deny non-compliant port range (Array)" -Tag "deny-route-nexthopvirtualappliance-nsg-port-60" {
+        It "Should deny non-compliant port range (Array)" -Tag "deny-noncompliant-nsg-port-60" {
             AzTest -ResourceGroup {
                 param($ResourceGroup)
 
@@ -206,7 +206,7 @@ Describe "Testing policy 'Deny-MgmtPorts-From-Internet'" -Tag "deny-mgmtports-fr
             }
         }
 
-        It "Should deny non-compliant port ranges (Array) - API" -Tag "deny-route-nexthopvirtualappliance-nsg-port-70" {
+        It "Should deny non-compliant port ranges (Array) - API" -Tag "deny-noncompliant-nsg-port-70" {
             AzTest -ResourceGroup {
                 param($ResourceGroup)
 
@@ -267,6 +267,70 @@ Describe "Testing policy 'Deny-MgmtPorts-From-Internet'" -Tag "deny-mgmtports-fr
                     throw "Operation failed with message: '$($httpResponse.Content)'"
                 }              
                 } | Should -Throw "*disallowed by policy*"
+            }
+        }
+
+        It "Should allow compliant port ranges (Array) - API" -Tag "allow-compliant-nsg-port-20" {
+            AzTest -ResourceGroup {
+                param($ResourceGroup)
+
+                # Should be disallowed by policy, so exception should be thrown.
+                {
+                    $payload = @"
+{
+    "properties": {
+        "securityRules": [
+            {
+                "name": "Web-rule",
+                "properties": {
+                    "description": "Allow Web2",
+                    "protocol": "Tcp",
+                    "sourcePortRange": "*",
+                    "destinationPortRange": "443",
+                    "sourceAddressPrefix": "*",
+                    "destinationAddressPrefix": "*",
+                    "access": "Allow",
+                    "priority": 300,
+                    "direction": "Inbound"
+                }
+            },
+            {
+                "name": "Multi-rule",
+                "properties": {
+                    "description": "Allow Mgmt3",
+                    "protocol": "Tcp",
+                    "sourcePortRange": "*",
+                    "destinationPortRanges": ["23","3390-3392","8080"],
+                    "sourceAddressPrefix": "*",
+                    "destinationAddressPrefix": "*",
+                    "access": "Allow",
+                    "priority": 310,
+                    "direction": "Inbound"
+                }
+            }
+        ]
+    },
+    "location": "uksouth"
+}
+"@
+
+                    $httpResponse = Invoke-AzRestMethod `
+                        -ResourceGroupName $ResourceGroup `
+                        -ResourceProviderName "Microsoft.Network" `
+                        -ResourceType "networkSecurityGroups" `
+                        -Name "testNSG99" `
+                        -ApiVersion "2022-11-01" `
+                        -Method "PUT" `
+                        -Payload $payload
+            
+                if ($httpResponse.StatusCode -eq 200 -or $httpResponse.StatusCode -eq 201) {
+                    # NSG created
+                }
+                # Error response describing why the operation failed.
+                else {
+                    throw "Operation failed with message: '$($httpResponse.Content)'"
+                }              
+                } | Should Not -Throw
             }
         }
     }
